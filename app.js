88eb5f3aa3ef90c3e5f83431759dc0be684f04ea
emartincek1 <evan.martincek@gmail.com>
2024-01-31 15:06:14 -0600

    add ejs and ejs-mate

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render("index");
 });
 
 app.listen(3000, () => {
